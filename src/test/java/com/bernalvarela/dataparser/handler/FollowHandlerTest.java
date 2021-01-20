ab92db55001e4f5b2b0d437e49c17492417c468a
Bernal Varela Gómez <bernalvg@ext.inditex.com>
2021-01-20 18:20:08 +0100

    Added info to test in FollowHandlerTest

--- src/test/java/com/bernalvarela/dataparser/handler/FollowHandlerTest.java
+++ src/test/java/com/bernalvarela/dataparser/handler/FollowHandlerTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.bernalvarela.dataparser.entity.ConnectionInfo;
 import com.bernalvarela.dataparser.executor.vo.ExecutorParams;
 import java.lang.reflect.Field;
+import java.util.Map;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,7 +20,8 @@ public class FollowHandlerTest {
       ExecutorParams executorParams,
       ConnectionInfo connectionInfo,
       String expectedHostnamesConnectedToGivenHost,
-      String hostnamesReceivedConnectionsFromGivenHost
+      String hostnamesReceivedConnectionsFromGivenHost,
+      Map<String, Integer> expectedConnectionsGenerated
   ) {
     FollowHandler followHandler = new FollowHandler();
     followHandler.doLogic(executorParams, connectionInfo);
@@ -28,6 +30,8 @@ public class FollowHandlerTest {
         .isEqualTo(expectedHostnamesConnectedToGivenHost);
     assertThat(getStringFieldValue(followHandler, "hostnamesReceivedConnectionsFromGivenHost").trim())
         .isEqualTo(hostnamesReceivedConnectionsFromGivenHost);
+    assertThat(getConnectionsInfoValue(followHandler, "connectionsGenerated")).isEqualTo(expectedConnectionsGenerated);
+
   }
 
   private Object getFieldValue(FollowHandler followHandler, String fieldName) throws NoSuchFieldException, IllegalAccessException {
@@ -40,5 +44,10 @@ public class FollowHandlerTest {
   @SneakyThrows
   private String getStringFieldValue(FollowHandler followHandler, String fieldName) {
     return (String) getFieldValue(followHandler, fieldName);
+  }
+
+  @SneakyThrows
+  private Map<String, Integer> getConnectionsInfoValue(FollowHandler followHandler, String fieldName) {
+    return (Map<String, Integer>) getFieldValue(followHandler, fieldName);
   }
 }
--- src/test/java/com/bernalvarela/dataparser/handler/FollowHandlerTestFixtures.java
+++ src/test/java/com/bernalvarela/dataparser/handler/FollowHandlerTestFixtures.java
@@ -3,6 +3,7 @@ package com.bernalvarela.dataparser.handler;
 import com.bernalvarela.dataparser.entity.ConnectionInfo;
 import com.bernalvarela.dataparser.executor.vo.ExecutorParams;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.Arguments;
 
@@ -22,25 +23,29 @@ public class FollowHandlerTestFixtures {
             ExecutorParams.builder().follow(true).hostNames(List.of(HAILEEN, RAMONDO)).build(),
             ConnectionInfo.builder().originHost(RAMONDO).destinationHost(HAILEEN).build(),
             RAMONDO,
-            HAILEEN
+            HAILEEN,
+            Map.of(RAMONDO, 1)
         ),
         Arguments.of(
             ExecutorParams.builder().follow(true).hostNames(List.of(RAMONDO, HAILEEN)).build(),
             ConnectionInfo.builder().originHost(RAMONDO).destinationHost(HAILEEN).build(),
             EMPTY_STRING,
-            EMPTY_STRING
+            EMPTY_STRING,
+            Map.of(RAMONDO, 1)
         ),
         Arguments.of(
             ExecutorParams.builder().follow(true).hostNames(List.of(RAMONDO, HAILEEN)).build(),
             ConnectionInfo.builder().originHost(SHILLA).destinationHost(RAMONDO).build(),
             SHILLA,
-            EMPTY_STRING
+            EMPTY_STRING,
+            Map.of(SHILLA, 1)
         ),
         Arguments.of(
             ExecutorParams.builder().follow(true).hostNames(List.of(RAMONDO, HAILEEN)).build(),
             ConnectionInfo.builder().originHost(HAILEEN).destinationHost(SHILLA).build(),
             EMPTY_STRING,
-            SHILLA
+            SHILLA,
+            Map.of(HAILEEN, 1)
         )
     );
   }
